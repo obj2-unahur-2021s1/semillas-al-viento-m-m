bbe4b48ce809f431158cfd6fa6b12856301b176e
marcarand <marcarand@riseup.net>
2021-05-11 11:35:34 -0300

    revisión final

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -3,7 +3,7 @@ package ar.edu.unahur.obj2.semillasAlViento
 abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
   fun esFuerte() = this.horasDeSolQueTolera() > 10
 
-  // Aquí hay un problema de abstracción: esta función debería estar en el módulo `Parcela`
+  // Aquí hay un problema de abstracción/cohesión: esta función debería estar en el módulo `Parcela`
   fun parcelaTieneComplicaciones(parcela: Parcela) =
     parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia }
 
@@ -13,9 +13,9 @@ abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
 
 class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemilla, altura) {
   override fun horasDeSolQueTolera() = 6
-  override fun daSemillas() = this.esFuerte() || altura > 0.4  // Esta función no es robusta porque genera un
-                                                               // comportamiento errático al introducir el
-                                                               // booleano de la condición 'esFuerte'
+  override fun daSemillas() = this.esFuerte() || altura > 0.4  // Esta función no es robusta porque genera
+                                                               // un comportamiento errático al introducir
+                                                               // el booleano de la condición 'esFuerte'
 }
 
 // no es cohesiva la clase soja
@@ -24,7 +24,7 @@ class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean)
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
 
-    //no seria flexible porq a medida que agregas mas condiciones se haria mas largo el codigo
+    // No sería flexible porque a medida que agregas más condiciones se haría más largo el código
     val horasBase = when {
       altura < 0.5  -> 6
       altura < 1    -> 7
@@ -35,13 +35,13 @@ class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean)
   }
 
   override fun daSemillas(): Boolean  {
-    // Esta función no es consistente con la función anterior, ya que aplica el mismo criterio de resolución
-    // para la condición de transgénica.
+    // Esta función no es consistente con la función anterior, ya que no aplica
+    // el mismo criterio de resolución del problema para la condición de transgénica.
     if (this.esTransgenica) {
       return false
     }
-    // Esta función no es robusta porque genera un comportamiento errático al introducir el
-    // booleano de la condición 'esFuerte'
+    // Esta parte de la función no es robusta porque genera un comportamiento errático
+    // al introducir el booleano de la condición 'esFuerte'
     return this.esFuerte() || (this.anioObtencionSemilla > 2007 && this.altura > 1)
   }
 }
