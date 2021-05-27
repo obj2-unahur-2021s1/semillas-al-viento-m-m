21b94e7e10cf058c09e3402490d21e2a1a15238a
unknown <tomasmiranda98@hotmail.com>
2021-05-27 20:19:05 -0300

    se agrega test si es fuerte la menta

--- src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/PlantaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/PlantaTest.kt
@@ -6,19 +6,23 @@ import io.kotest.matchers.shouldBe
 
 class PlantaTest: DescribeSpec({
 
-    describe("requerimientos Menta"){
+    describe("requerimientos Menta") {
         //se cambia el tipo de dato de altura de float a int, ya que salta "The integer literal does not conform to the expected type Float"
-        val menta = Menta(2021,6)
-        describe("horas de sol de la menta"){
-            it("las horas de sol de la menta es de 6 horas"){
+        val menta = Menta(2021, 6)
+        describe("horas de sol de la menta") {
+            it("las horas de sol de la menta es de 6 horas de sol") {
                 menta.horasDeSolQueTolera().shouldBe(6)
             }
-            it("las horas de sol de la menta nunca sera distinto de 6 horas "){
-                (menta.horasDeSolQueTolera()==10).shouldBeFalse()
+            it("las horas de sol de la menta nunca sera distinto de 6 horas de sol") {
+                (menta.horasDeSolQueTolera() == 10).shouldBeFalse()
+            }
+        }
+        describe("es fuerte la menta") {
+            it("la menta no es fuerte porq no tolera mas de 6 horas de sol") {
+                menta.esFuerte().shouldBeFalse()
             }
         }
     }
-
     describe("requerimientos Soja"){}
 
     describe("requerimientos Soja Transgenica"){}
