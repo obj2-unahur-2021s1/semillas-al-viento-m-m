129efcd5deb4ceb0023c33d7b18a022efeb2efd0
unknown <tomasmiranda98@hotmail.com>
2021-05-27 19:48:22 -0300

    se agrega archivos de test para la semillas

---
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/PlantaTest.kt
@@ -0,0 +1,16 @@
+package ar.edu.unahur.obj2.semillasAlViento
+
+import io.kotest.core.spec.style.DescribeSpec
+
+class PlantaTest: DescribeSpec({
+
+    describe("requerimientos Menta"){}
+
+    describe("requerimientos Soja"){}
+
+    describe("requerimientos Soja Transgenica"){}
+
+    describe("requerimientos Parcela"){}
+
+    describe("requerimientos Agricultora"){}
+})
