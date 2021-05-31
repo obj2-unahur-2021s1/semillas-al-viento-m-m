01cddaf66247662d70df977576ca241aa897716c
marcarand <marcarand@riseup.net>
2021-05-30 23:02:04 -0300

    test para características de la parcela

--- src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/ParcelaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/ParcelaTest.kt
@@ -4,16 +4,28 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class ParcelaTest() : DescribeSpec( {
-    val miParcela = Parcela(25, 50, 10)
+
+    val miParcela = Parcela(25, 50, 8)
+    val menta = Menta(2020, 0.48)
+    val soja = Soja(2019, 1.70, false)
+
     describe("Características de la parcela") {
         it("La superficie es 1250 m2") {
             miParcela.superficie().shouldBe(1250)
         }
-        it("cantidad máxima de plantas toleradas es XX") {
-            // TODO: 30/5/21
+        it("cantidad máxima de plantas toleradas es 466") {
+            miParcela.cantidadMaximaPlantas().shouldBe(466)
         }
-        it("La parcela tiene complicaciones")
+        it("La parcela no tiene complicaciones") {
+            miParcela.plantar(soja)
+            soja.parcelaTieneComplicaciones(miParcela).shouldBe(false)
+        }
+        it("La parcela tiene complicaciones") {
+            miParcela.plantar(menta)
+            menta.parcelaTieneComplicaciones(miParcela).shouldBe(true)
+        }
     }
+
     describe("Acciones en la parcela") {
         it("Se puede plantar una plata") {
             // TODO: 30/5/21
