8d099b78c055a9865d586f55099c4166cec41d51
unknown <tomasmiranda98@hotmail.com>
2021-06-03 17:47:08 -0300

    se soluciona tests de agricultores

--- src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/AgricultorTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/AgricultorTest.kt
@@ -11,44 +11,43 @@ class AgricultorTest: DescribeSpec( {
     val miParcela = Parcela(25, 50, 9)
     val miParcela2 = Parcela(25, 50, 9)
 
-    val menta = Menta(2020, 0.48)
     val sojaGrande = Soja(2019, 1.70)
+    val sojaGrande2 = Soja(2019, 1.70)
     val sojaChica = Soja(2020, 0.6)
+    val sojaTransgenica = SojaTransgenica(2002,0.6)
 
-    //miParcela.plantar(menta)
-    //miParcela.plantar(sojaGrande)
+    miParcela.plantar(sojaGrande)
+    miParcela.plantar(sojaGrande2)
 
-    //miParcela2.plantar(menta)
-    //miParcela2.plantar(sojaChica)
-    //miParcela2.plantar(sojaTransgenica)
-    /*
+    miParcela2.plantar(sojaChica)
+    miParcela2.plantar(sojaTransgenica)
+
     val listaParcela = mutableListOf<Parcela>()
     listaParcela.add(miParcela)
     listaParcela.add(miParcela2)
-    */
-    //val agricultora = Agricultora(listaParcela)
+
+    val agricultora = Agricultora(listaParcela)
 
     describe("parcelas semilleras"){
 
         it("la parcela 1 es la unica semillera de la agricultura es la miParcela"){
-            menta.daSemillas().shouldBeTrue()
-            /*
             sojaGrande.daSemillas().shouldBeTrue()
+
 
             agricultora.parcelasSemilleras().size.shouldBe(1)
             agricultora.parcelasSemilleras().shouldContain(miParcela)
-            */
+
         }
-        /*
+
         it("la parcela 2 no es semillera"){
             agricultora.parcelasSemilleras().shouldNotContain(miParcela2)
         }
-         */
+
     }
 
     describe("plantar estrategicamente"){
         val soja = Soja(2021, 1.70)
-        /*
+
         it("se planta en la parcela 1"){
             soja.daSemillas().shouldBeTrue()
 
@@ -57,7 +56,7 @@ class AgricultorTest: DescribeSpec( {
             agricultora.parcelas[0].plantas.shouldContain(soja)
         }
 
-         */
+
     }
 
 })
