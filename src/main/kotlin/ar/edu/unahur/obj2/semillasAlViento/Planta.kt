2440292ed48ab1a7c4d8da1e119d1098b6070ca6
unknown <tomasmiranda98@hotmail.com>
2021-06-08 19:15:25 -0300

    se refactoriza en template method

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -3,13 +3,13 @@ package ar.edu.unahur.obj2.semillasAlViento
 abstract class Planta(val anioObtencionSemilla: Int, val altura: Double) {
   fun esFuerte() = this.horasDeSolQueTolera() > 10
   abstract fun horasDeSolQueTolera(): Int
-  abstract val factorAdicional : Boolean
-  open fun daSemillas(): Boolean = this.esFuerte() || factorAdicional
+  abstract fun factorAdicional() : Boolean
+  open fun daSemillas(): Boolean = this.esFuerte() || this.factorAdicional()
 }
 
 class Menta(anioObtencionSemilla: Int, altura: Double) : Planta(anioObtencionSemilla, altura) {
   override fun horasDeSolQueTolera() = 6
-  override val factorAdicional = altura > 0.4
+  override fun factorAdicional() = altura > 0.4
 }
 
 open class Soja(anioObtencionSemilla: Int, altura: Double) : Planta(anioObtencionSemilla, altura) {
@@ -22,7 +22,7 @@ open class Soja(anioObtencionSemilla: Int, altura: Double) : Planta(anioObtencio
     return horasBase
   }
 
-  override val factorAdicional = this.anioObtencionSemilla > 2007 && this.altura > 1
+  override fun factorAdicional() = this.anioObtencionSemilla > 2007 && this.altura > 1
 }
 class SojaTransgenica(anioObtencionSemilla: Int, altura: Double): Soja(anioObtencionSemilla, altura){
   override fun horasDeSolQueTolera() = super.horasDeSolQueTolera()*2
